5149c5279094ce7cdc507221bbbe5e769830457b
Shivani Shah <shivanishah96@hotmail.co.uk>
2020-08-18 10:22:24 +0100

    add understanding of psst.js

--- exercise-2/psst.js
+++ exercise-2/psst.js
@@ -6,6 +6,13 @@ module.exports = (secret) => {
     throw Error("invalid secret!");
   }
   //HMAC: hashed-based message authenication code
+  //generate HMAC sha256hash, which also takes in a secret which is a string
+  //.update and .digest methods produce the hmac digest
+  //.update(data[,inputEncoding]) - Updates the Hmac content with the given data, the encoding of which is given
+  //    in inputEncoding.If encoding is not provided, and the data is a string, an encoding of
+  //    'utf8' is enforced.
+  //.digest([encoding])  - Calculates the HMAC digest of all of the data passed using hmac.update().
+  //    If encoding is provided a string is returned; otherwise a Buffer is returned;
   const functions = {
     sign: (value) => {
       return crypto.createHmac("sha256", secret).update(value).digest("hex");
